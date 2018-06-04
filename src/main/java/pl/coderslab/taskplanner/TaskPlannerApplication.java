591e246a6e7f1c31c79fbae0b71d860e7d46edfe
Mateusz Kotowski <kotas55@wp.pl>
2018-06-04 14:36:39 +0200

    start project

---
+++ src/main/java/pl/coderslab/taskplanner/TaskPlannerApplication.java
@@ -0,0 +1,12 @@
+package pl.coderslab.taskplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskPlannerApplication.class, args);
+    }
+}
